d2d2180f61eff705b7b4c87775c309a5b23095d4
salmankazmi <slmnkzm@gmail.com>
2020-06-06 17:27:34 +0530

    added font awesome and js mods

--- index.js
+++ index.js
@@ -4,15 +4,32 @@ import './style.css';
 // Write Javascript code!
 const addButton = document.getElementById('add-task');
 const taskInput = document.getElementById('task-input');
+const taskList = document.querySelector('ul');
+const previousValue = '';
 
 function main() {
   addButton.addEventListener('click', () => {
     const task = taskInput.value;
-    const taskList = document.querySelector('ul');
-    const newTask = document.createElement('li');
-    newTask.textContent = task;
-    taskList.append(newTask);
+    addTask(task);
   })
+}
+
+function addTask(task) {
+  if (task && String(task).trim().length > 0) {
+    if (previousValue !== task) {
+      const newTask = document.createElement('li');
+      const taskTextBox = document.createElement('input');
+      const removeTaskIcon = document.createElement('span');
+      removeTaskIcon.innerHTML = `
+        <i class="far fa-trash-alt"></i>
+      `;
+      taskTextBox.value = task;
+      newTask.append(taskTextBox);
+      newTask.append(removeTaskIcon);
+      taskList.append(newTask);
+      previousValue = task;
+    }
+  }
 }
 
 main();
